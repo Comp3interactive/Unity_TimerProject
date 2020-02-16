a507d490dfed4981ac8ca7e915c1eef082008885
Comp-3 Interactive <Comp3interactive@users.noreply.github.com>
2020-02-16 15:37:20 +0000

    Initial Commit

---
+++ Assets/Tutorial Assets/Scripts/Timer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    [Header("Time Values")]
+    [Range(0,60)]
+    public int seconds;
+    [Range(0, 60)]
+    public int minutes;
+    [Range(0, 60)]
+    public int hours;
+
+    public Color fontColor;
+
+    public bool showMilliseconds;
+
+    private float currentSeconds;
+    private int timerDefault;
+
+    void Start()
+    {
+        timerText.color = fontColor;
+        timerDefault = 0;
+        timerDefault += (seconds + (minutes * 60) + (hours * 60 * 60));
+        currentSeconds = timerDefault;
+    }
+
+    void Update()
+    {
+        if((currentSeconds -= Time.deltaTime) <= 0)
+        {
+            TimeUp();
+        }
+        else
+        {
+            if(showMilliseconds)
+                timerText.text = TimeSpan.FromSeconds(currentSeconds).ToString(@"hh\:mm\:ss\:fff");
+            else
+                timerText.text = TimeSpan.FromSeconds(currentSeconds).ToString(@"hh\:mm\:ss");
+        }
+    }
+
+    private void TimeUp()
+    {
+        if (showMilliseconds)
+            timerText.text = "00:00:00:000";
+        else
+            timerText.text = "00:00:00";
+    }
+}
